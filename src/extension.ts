6eb272ad69ca426ebbde1d09d81d8b09b26c82a0
ZZYSonny <zhaozy5520@outlook.com>
2024-01-17 08:48:17 +0000

    tree view

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bookmarkfordebugging" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bookmarkfordebugging.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from BookmarkForDebugging!');
-	});
-
-	context.subscriptions.push(disposable);
+class Dependency extends vscode.TreeItem {
+	constructor() {
+		super("label", 1);
+	}
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
+	constructor() { }
+
+	getTreeItem(element: Dependency): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: Dependency): Thenable<Dependency[]> {
+		if (!element) {
+			return Promise.resolve([new Dependency()]);
+		} else {
+			return Promise.resolve([new Dependency()]);
+		}
+	}
+}
+
+const bookmarkProvider = new NodeDependenciesProvider();
+const bookmarkView = vscode.window.createTreeView("bugmark.view.bookmarks", { treeDataProvider: bookmarkProvider });
+vscode.commands.registerCommand('bugmark.command.markline', () => {
+	// The code you place here will be executed every time your command is executed
+	// Display a message box to the user
+	let editor = vscode.window.activeTextEditor;
+	let document = editor.document;
+	let cursor = editor.selection.active;
+	let file = document.fileName;
+	let line = document.lineAt(cursor).lineNumber;
+	vscode.window.showInformationMessage(`Bookmark ${line}`);
+	//vscode.debug.addBreakpoints
+})
+
+//context.subscriptions.push();
+export function activate(context: vscode.ExtensionContext) { }
+export function deactivate() { }
