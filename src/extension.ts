552370f5aa1d8f9f157cf06a1bbf8dee941eb95c
ZZYSonny <zhaozy5520@outlook.com>
2024-01-20 11:29:28 +0000

    tree view

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,83 @@
 import * as vscode from 'vscode';
 
-class Dependency extends vscode.TreeItem {
-	constructor() {
-		super("label", 1);
+class RecordProp {
+	file: string
+	lineno: number
+	content: string
+	commit: string
+	head: boolean
+}
+
+type RecordLeaf = Array<RecordProp>
+type RecordParent = Map<string, RecordNode>
+type RecordNode = RecordLeaf | RecordParent
+
+let RecordRoot: RecordNode = new Map([
+	["1", new Map([
+		["line 1", [{
+			file: "1.js",
+			lineno: 0,
+			content: "line 1",
+			commit: "",
+			head: true
+		}]],
+		["line 2", [{
+			file: "1.js",
+			lineno: 1,
+			content: "line 2",
+			commit: "",
+			head: true
+		}]],
+	])]
+])
+
+class RecordItem extends vscode.TreeItem {
+	constructor(name: string, public node: RecordNode) {
+		let collapseState = (node instanceof Map)?2:0;
+		super(name, collapseState);
 	}
 }
 
-export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
+export class BugMarkTreeProvider implements vscode.TreeDataProvider<RecordItem> {
 	constructor() { }
 
-	getTreeItem(element: Dependency): vscode.TreeItem {
+	getTreeItem(element: RecordItem): vscode.TreeItem {
 		return element;
 	}
 
-	getChildren(element?: Dependency): Thenable<Dependency[]> {
-		if (!element) {
-			return Promise.resolve([new Dependency()]);
+	getChildren(element?: RecordItem): Thenable<RecordItem[]> {
+		let node: RecordNode = element ? element.node : RecordRoot;
+		if (node instanceof Map) {
+			return Promise.resolve(
+				Array.from(node).map(
+					([name, child]) => new RecordItem(name, child))
+			)
 		} else {
-			return Promise.resolve([new Dependency()]);
+			throw "Bugmark: getChildren on a leaf node";
 		}
 	}
 }
 
-const bookmarkProvider = new NodeDependenciesProvider();
-const bookmarkView = vscode.window.createTreeView("bugmark.view.bookmarks", { treeDataProvider: bookmarkProvider });
-vscode.commands.registerCommand('bugmark.command.markline', () => {
-	// The code you place here will be executed every time your command is executed
-	// Display a message box to the user
-	let editor = vscode.window.activeTextEditor;
-	let document = editor.document;
-	let cursor = editor.selection.active;
-	let file = document.fileName;
-	let line = document.lineAt(cursor).lineNumber;
-	vscode.window.showInformationMessage(`Bookmark ${line}`);
-	//vscode.debug.addBreakpoints
-})
+let provider = new BugMarkTreeProvider();
 
-//context.subscriptions.push();
-export function activate(context: vscode.ExtensionContext) { }
-export function deactivate() { }
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.window.registerTreeDataProvider(
+		"bugmark.view.bookmarks", provider
+	))
+	context.subscriptions.push(vscode.commands.registerCommand(
+		'bugmark.command.markline',
+		() => {
+			let editor = vscode.window.activeTextEditor;
+			let document = editor.document;
+			let cursor = editor.selection.active;
+			let file = document.fileName;
+			let line = document.lineAt(cursor).lineNumber;
+			vscode.window.showInformationMessage(`Bookmark ${line}`);
+			//vscode.debug.addBreakpoints
+		}
+	))
+}
+export function deactivate() {
+	RecordRoot = null;
+	provider = null;
+}
